85914849b9b20826b2aee8739c53a0c8c9fb60a0
victor Simonet <102629039+simonet4@users.noreply.github.com>
2026-03-29 13:10:14 +0200

    Revert "Improve routing and view loading with 404"
    This reverts commit 6fb9a3dacb39d6e872ad65921970507ef07dfc84.

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ session_start();
 $isLoginRoute = strtok($_SERVER["REQUEST_URI"], '?') === '/login';
 if (!$isLoginRoute && !isset($_SESSION['auth_token'])) {
     header('Location: /login');
-    exit();
 }
 ?>
 
@@ -51,27 +50,7 @@ if (!$isLoginRoute && !isset($_SESSION['auth_token'])) {
         </nav>
     <?php endif; ?>
     <?php
-    // On récupère l'URI sans les paramètres GET (?id=...)
-    $uri = strtok($_SERVER["REQUEST_URI"], '?');
-
-    // Si l'utilisateur est à la racine, on définit une page par défaut
-    if ($uri === '/' || $uri === '') {
-        $uri = '/tableau_de_bord';
-    }
-
-    // 3. On construit le chemin du fichier
-    $chemin_vue = './Vue' . $uri . '.php';
-
-    // fichier existe ? on l'inclut, sinon 404
-    if (file_exists($chemin_vue)) {
-        require_once $chemin_vue;
-    } else {
-        // erreur 404 si le fichier n'existe pas
-        http_response_code(404);
-        echo "<h1>Erreur 404</h1><p>La page demandée n'existe pas.</p>";
-    }
-    ?>
+        require_once './Vue' . strtok($_SERVER["REQUEST_URI"],'?') . '.php';
+    } ?>
     </body>
 </html>
-<?php
-}
